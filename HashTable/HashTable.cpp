c955f83323e4f6c3d5360d6fc45d55975bf7da6c
Ioannis_Goulias <jgcaptain95@hotmail.com>
2017-06-06 12:46:40 +0300

    Till contains

--- HashTable/HashTable.cpp
+++ HashTable/HashTable.cpp
@@ -24,3 +24,62 @@ HashTable::HashTable(int capacity) {
 }
 
 
+HashTable::HashTable(const HashTable &ht) {
+    this->capacity = ht.capacity;
+    this->table = new string[this->capacity];
+    this->size = ht.size;
+    
+    if(ht.size != 0) {
+        for (int i = 0; i < ht.capacity; i++) {
+            if(ht.table[i] != "") {
+                this->table[i] = ht.table[i];
+            }
+        }
+    }
+    
+    
+}
+
+
+//Methods
+
+int HashTable::getSize() {
+    return this->size;
+}
+
+int HashTable::getCapacity() {
+    return this->capacity;
+}
+
+bool HashTable::isEmpty(int pos) {
+    if (pos >= this->capacity) return false;
+    
+    if(this->table[pos] == "") {
+        return true;
+    }
+    
+    return false;
+}
+
+
+bool HashTable::isTomb(int pos) {
+    if(pos >= this->capacity) return false;
+    
+    if(this->table[pos].compare("##tomb##") != 0) return false;
+    return true;
+}
+
+
+bool HashTable::isAvailable(int pos) {
+    if(isEmpty(pos) | isTomb(pos)) return true;
+    return false;
+}
+
+bool HashTable::conta
+
+
+
+
+
+
+
